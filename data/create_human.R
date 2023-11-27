361b2180ad981962ea3bd2e51ac607b5f4e79757
Oskar Lappi <oskar.lappi@abo.fi>
2023-11-28 01:30:23 +0200

    fixed bug in create_human.R

--- data/create_human.R
+++ data/create_human.R
@@ -3,6 +3,7 @@
 # ---- read
 
 library(readr)
+library(dplyr)
 hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
 gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv", na = "..")
 
